d65383db091235954122be7c98c4eb9ad41977b5
Arda Ozceviz <ardaozceviz@hotmail.com>
2017-10-06 16:21:49 +0300

    working with exceptions

---
+++ src/Basics/Exceptions.kt
@@ -0,0 +1,24 @@
+package Basics
+
+class NotANumberException(message: String) : Throwable(message)
+
+fun checkIsNumber(obj: Any){
+    when(obj){
+        !is Int, Double, Float, Long -> throw NotANumberException("This is not a number")
+    }
+}
+
+fun main(args: Array<String>) {
+    try {
+        checkIsNumber("arda")
+    }
+    catch (e: IllegalArgumentException){
+        println("Do nothing")
+    }
+    catch (e: NotANumberException){
+        // println("It was not a number")
+        println("${e.message}")
+    } finally { // finally block, if finalises even exception is thrown. anything bad can happen this code will still execute
+        println("This is from final part")
+    }
+}
